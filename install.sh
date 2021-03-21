eeac220ac03d9e8e69de9db6d9e35b9bd4c36d3f
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-03-21 17:17:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,14 +58,8 @@ sudo cat <<EOF > /usr/local/nginx/conf/nginx.conf
 
 #############################################################################
 
-#user  nobody;
 worker_processes  auto;
-
 #error_log  logs/error.log;
-#error_log  logs/error.log  notice;
-#error_log  logs/error.log  info;
-
-pid   /var/run/nginx.pid;
 
 events {
     worker_connections  1024;
@@ -76,24 +70,49 @@ rtmp {
     server {
         listen 1935;         # Listen on standard RTMP port
         chunk_size 4000;
+	# ping 30s;
+	# notify_method get;
 
-        application stream {
-            live on;
-            record off;
+        application live {
+            live on;         # Allows live input
+	    
+	    # for each received stream, transcode for adaptive streaming
+			# This single ffmpeg command takes the input and transforms
+			# the source into 4 different streams with different bitrates
+			# and qualities. # these settings respect the aspect ratio.
+			exec_push  /usr/local/bin/ffmpeg -i rtmp://localhost:1935/$app/$name -async 1 -vsync -1
+						-c:v libx264 -c:a aac -b:v 256k  -b:a 64k  -vf "scale=480:trunc(ow/a/2)*2"  -tune zerolatency -preset superfast -crf 23 -f flv rtmp://localhost:1935/show/$name_low
+						-c:v libx264 -c:a aac -b:v 768k  -b:a 128k -vf "scale=720:trunc(ow/a/2)*2"  -tune zerolatency -preset superfast -crf 23 -f flv rtmp://localhost:1935/show/$name_mid
+						-c:v libx264 -c:a aac -b:v 1024k -b:a 128k -vf "scale=960:trunc(ow/a/2)*2"  -tune zerolatency -preset superfast -crf 23 -f flv rtmp://localhost:1935/show/$name_high
+						-c:v libx264 -c:a aac -b:v 1920k -b:a 128k -vf "scale=1280:trunc(ow/a/2)*2" -tune zerolatency -preset superfast -crf 23 -f flv rtmp://localhost:1935/show/$name_hd720
+						-c copy -f flv rtmp://localhost:1935/show/$name_src;
+		}
 
-            hls on;
-            hls_path /usr/local/nginx/html/stream/hls;
+        # This is the HLS application
+        application show {
+	    live on;          # Allows live input from above application
+	    deny play all;    # disable consuming the stream from nginx as rtmp
+			
+            hls on;                                            # Enable HTTP Live Streaming
+            hls_path /usr/local/nginx/html/stream/hls;         # hls fragments path
             hls_nested on;
             hls_fragment 2s;
             hls_playlist_length 16s;
+	    
+	    # Instruct clients to adjust resolution according to bandwidth
+	    hls_variant _src BANDWIDTH=4096000;                # Source bitrate, source resolution
+	    hls_variant _hd720 BANDWIDTH=2048000;              # High bitrate, HD 720p resolution
+	    hls_variant _high BANDWIDTH=1152000;               # High bitrate, higher-than-SD resolution
+	    hls_variant _mid BANDWIDTH=448000;                 # Medium bitrate, SD resolution
+	    hls_variant _low BANDWIDTH=288000;                 # Low bitrate, sub-SD resolution
 
+            # This is the Dash application
             dash on;
-            dash_path /usr/local/nginx/html/stream/dash;
+            dash_path /usr/local/nginx/html/stream/dash;       # dash fragments path
             dash_nested on;
             dash_fragment 2s;
             dash_playlist_length 16s;
 
-            # deny play all;
         }
     }
 }
@@ -108,7 +127,8 @@ http  {
     
                 include mime.types;
                 default_type application/octet-stream;
-
+		
+    # HTTP server required to serve the player and HLS fragments
     server {
                 listen 443;
                 server_name example.com;
@@ -120,7 +140,20 @@ http  {
 				video/mp2t ts;
 			}
 			        root /usr/local/nginx/html/stream;
-                                add_header Cache-Control no-cache;       
+                                add_header Cache-Control no-cache;       # Disable cache
+				
+				# CORS setup
+                                add_header 'Access-Control-Allow-Origin' '*' always;
+                                add_header 'Access-Control-Expose-Headers' 'Content-Length';
+				
+				# Allow CORS preflight requests
+                                if ($request_method = 'OPTIONS') {
+                                     add_header 'Access-Control-Allow-Origin' '*';
+                                     add_header 'Access-Control-Max-Age' 1728000;
+                                     add_header 'Content-Type' 'text/plain charset=UTF-8';
+                                     add_header 'Content-Length' 0;
+                                     return 204;
+                               }
 		        }
 		
                  # Serve DASH fragments
@@ -131,23 +164,31 @@ http  {
                         }
 
 		                 root /usr/local/nginx/html/stream;
-                                 add_header Cache-Control no-cache;
+                                 add_header Cache-Control no-cache;      # Disable cache
+				 
+				 # CORS setup
+                                add_header 'Access-Control-Allow-Origin' '*' always;
+                                add_header 'Access-Control-Expose-Headers' 'Content-Length';
+				
+				# Allow CORS preflight requests
+                                if ($request_method = 'OPTIONS') {
+                                     add_header 'Access-Control-Allow-Origin' '*';
+                                     add_header 'Access-Control-Max-Age' 1728000;
+                                     add_header 'Content-Type' 'text/plain charset=UTF-8';
+                                     add_header 'Content-Length' 0;
+                                     return 204;
+                               }
                         }		
 		
 		        # This URL provides RTMP statistics in XML
 		        location /stat {
 			         rtmp_stat all;
-
-                                 # Use this stylesheet to view XML as web page
-                                 # in browser
-                                 rtmp_stat_stylesheet stat.xsl; 
+                                 rtmp_stat_stylesheet stat.xsl;     # Use stat.xsl stylesheet
 		        }
 
 		        location /stat.xsl {
 			         # XML stylesheet to view RTMP stats.
-                                 # Copy stat.xsl wherever you want
-                                 # and put the full directory path here
-                                 root /path/to/stat.xsl/;
+                                 root /usr/local/nginx/html;
 		        } 
 	          }
            }
